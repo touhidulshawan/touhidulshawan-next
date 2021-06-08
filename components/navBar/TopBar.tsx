5424ab82b79b5a723671cad7e11d768088ba76f6
Touhidul Shawan <touhidulshawan@gmail.com>
2021-06-08 10:32:35 +0600

    add link name github

--- components/navBar/TopBar.tsx
+++ components/navBar/TopBar.tsx
@@ -25,7 +25,7 @@ const TopBar = () => {
         )}
       </div>
       <Link href="https://github.com/touhidulshawan">
-        <a target="_blank" rel="noopener noreferrer">
+        <a target="_blank" rel="noopener noreferrer" aria-label="GitHub">
           <AiFillGithub className="w-7 h-7 border-2 border-blue-500 rounded-full  animate-pulse focus:outline-none focus:ring-4 dark:border-blue-300 dark:text-gray-300 " />
         </a>
       </Link>
