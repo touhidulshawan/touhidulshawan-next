64cc508e4fe565a968bc1bd44db350dda432fd7c
Touhidul Shawan <touhidulshawan@gmail.com>
2021-06-08 10:49:01 +0600

    add meta data

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,7 @@ const Layout: React.FC<LayoutProps> = ({ title, children }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <meta name="theme-color" />
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
