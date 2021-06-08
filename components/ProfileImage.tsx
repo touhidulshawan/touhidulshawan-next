e97d63f857586616784446148390feed523882b8
Touhidul Shawan <touhidulshawan@gmail.com>
2021-06-08 09:46:42 +0600

    change profile image

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 
 const ProfileImg: React.FC = () => {
   return (
-    <div className="w-64 h-64  mx-auto my-3  md:mt-4 lg:mt-10 lg:ml-auto  md:w-72 md:h-72 overflow-hidden  rounded-lg shadow-inner  border-r-8 border-b-4 border-t-2 border-l-2  border-gray-900 dark:border-blue-100">
+    <div className="w-64 h-64  mx-auto my-3  md:mt-4 lg:mt-10 lg:ml-auto  md:w-80 md:h-80 overflow-hidden  rounded-lg shadow-inner  border-r-8 border-b-4 border-t-2 border-l-2  border-gray-900 dark:border-blue-100">
       <Image
-        priority
-        layout="fixed"
-        src="/img/Portfolio.jpg"
+        // priority
+        objectFit="cover"
+        // layout="fill"
+        src="/img/Shawan-Potrait.svg"
         alt="portfolio img"
-        width={290}
-        height={290}
+        width={350}
+        height={360}
       />
     </div>
   );
